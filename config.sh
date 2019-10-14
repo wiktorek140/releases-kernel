b88c13d83ffd9597192b70349b0e8048ef74bfee
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-15 02:35:45 +0300

    Cereus-ify
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export ARCH=""
-export device=""
-export device_is_ab=""
+export ARCH="arm"
+export device="cereus"
+export device_is_ab="false"
 
-export kernel=""
-export branch=""
-export name=""
-export defconfig="_defconfig"
+export kernel="https://github.com/xiaomi-mt6765/android_kernel_xiaomi_mt6765"
+export branch="lineage-16.0"
+export name="LineageOS Kernel for cereus"
+export defconfig="cereus_defconfig"
 export clang="false"
