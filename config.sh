c434c988d482c99856808a200a6e41565b5e69fc
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-15 02:35:45 +0300

    sanders-ify
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export ARCH=""
-export device=""
-export device_is_ab=""
+export ARCH="arm64"
+export device="sanders"
+export device_is_ab="false"
 
-export kernel=""
-export branch=""
-export name=""
-export defconfig="_defconfig"
+export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
+export branch="lineage-17.1"
+export name="LineageOS Kernel for sanders"
+export defconfig="sanders_defconfig"
 export clang="false"
-export clang_version="r370808"
+export clang_version="r365631c"
