28f80e5a5e5feb43e6eb7d52e99c8a5d8e97c102
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-14 13:03:01 +0300

    hannah
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export ARCH="arm64"
-export device="montana"
+export device="hannah"
 export device_is_ab="false"
 
 export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
-export branch="lineage-17.1"
-export name="LineageOS Kernel for montana"
-export defconfig="montana_defconfig"
+export branch="rebase"
+export name="Audio test LineageOS Kernel for hannah"
+export defconfig="hannah_defconfig"
 export clang="false"
 export clang_version="r365631c"
