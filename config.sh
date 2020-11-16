1af57ca2cf5b3c2e4aa0572743e21c2e3d7e72cd
Wiktor Strzębała <wiktorek140@tlen.pl>
2020-11-16 19:55:12 +0100

    use other branch as build target

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export device="montana"
 export device_is_ab="false"
 
 export kernel="https://github.com/wiktorek140/android_kernel_motorola_msm8953"
-export branch="lineage-17.1"
+export branch="staging/rebase"
 export name="LineageOS Kernel for montana"
 export defconfig="montana_defconfig"
 export clang="false"
