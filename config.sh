f7913eaf1e8312fdb702f4eb07fd80ffae822eb0
Wiktor Strzębała <wiktorek140@tlen.pl>
2021-01-09 19:07:31 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,3 +10,5 @@ export name="LineageOS Kernel for montana"
 export defconfig="montana_defconfig"
 export clang="false"
 export clang_version="r365631c"
+
+export commits="4dcb1f152f2fec75500f81154cbfa8a2df271b4c ac069951b776f3aed88695a674cfe0a4f572f270 6d9925d839b24a3ef8ad1fdeacc381e2e9e51831"
